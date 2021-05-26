95ed2ea2398bf122ff85f1ce7e909204dc1f8bfd
cg-a11y <84869924+cg-a11y@users.noreply.github.com>
2021-05-26 15:02:02 -0400

    Removes analytics

--- themes/default/include/header.tmpl.php
+++ themes/default/include/header.tmpl.php
@@ -74,20 +74,7 @@ $lang_charset = "UTF-8";
 	<![endif]-->
 	<script src="<?php echo $base_path; ?>jscripts/lib/jquery.js" type="text/javascript"></script>
 	<script src="<?php echo $base_path; ?>jscripts/lib/jquery-URLEncode.js" type="text/javascript"></script>
-	<script src="<?php echo $base_path; ?>jscripts/AChecker.js" type="text/javascript"></script>   
-
-	<script type="text/javascript">
-	  var _paq = window._paq || [];
-	  _paq.push(['trackPageView']);
-	  _paq.push(['enableLinkTracking']);
-	  (function() {
-	    var u="https://analytics.inclusivedesign.ca/";
-	    _paq.push(['setTrackerUrl', u+'matomo.php']);
-	    _paq.push(['setSiteId', '6']);
-	    var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
-	    g.type='text/javascript'; g.async=true; g.defer=true; g.src=u+'matomo.js'; s.parentNode.insertBefore(g,s);
-	  })();
-	</script>
+	<script src="<?php echo $base_path; ?>jscripts/AChecker.js" type="text/javascript"></script>
 
 	<?php echo $rtl_css; ?>
 	<?php echo $custom_head; ?>
